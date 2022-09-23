f0d914c3585be776d8aa708521a849674aa4d950
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2022-09-23 18:21:35 +0300

    settime out

---
+++ index.js
@@ -0,0 +1,13 @@
+// ASYNCHRONOUS PROGRAMMING
+
+let color = 'white'
+
+let user = {
+    name:'kazi',
+    age :30
+};
+
+let friends = ['jack', 'Raju'];
+
+
+
---
+++ setimeout.js
@@ -0,0 +1,94 @@
+// example 1
+const Timeout = setTimeout(myGreeting, 5000);
+
+function myGreeting() {
+    console.log("react developer John")
+}
+
+myGreeting();
+
+// example 2
+
+const NodeTimeout = setTimeout(nodedev, 6000);
+
+function nodedev() {
+    console.log("node developer")
+}
+
+nodedev();
+
+// example 3
+const VueTimeout = setTimeout(vuedev, 7000);
+
+function vuedev() {
+    console.log("vue developer");
+}
+
+vuedev();
+
+// example 4
+const AngularTimeout = setTimeout(angulardev, 8000);
+
+function angulardev() {
+    console.log("angualr developer");
+}
+angulardev();
+
+// example 5
+const rubyTimeout = setTimeout(rubydev, 9000);
+
+function rubydev() {
+    console.log("ruby developer");
+}
+
+rubydev();
+
+// example 6
+
+const rustTimeout = setTimeout(rustdev, 10000);
+
+function rustdev() {
+    console.log("rust developer");
+}
+
+rustdev();
+
+// example 7
+
+const flaskTimeout = setTimeout(flaskdev, 20000);
+
+function flaskdev() {
+    console.log("flask developer");
+}
+
+flaskdev();
+
+
+// example 8
+
+const flutterTimeout = setTimeout(flutterdev, 30000);
+
+function flutterdev() {
+    console.log("flutter developer");
+}
+flutterdev();
+
+// example 9
+
+const iosTimeout = setTimeout(iosdev, 40000);
+
+function iosdev() {
+    console.log("ios developer");
+}
+iosdev();
+
+// example 10
+
+const javaTimeout = setTimeout(javadev, 50000);
+
+function javadev() {
+    console.log("java developer");
+}
+
+
+javadev();
